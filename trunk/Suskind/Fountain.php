7fb8535973cbf572d4d6e98fc4c7d185d7ddc5a1
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-01 14:15:23 +0000

    - Automatical path recognisation added.

--- trunk/Suskind/Fountain.php
+++ trunk/Suskind/Fountain.php
@@ -19,9 +19,9 @@ final class Suskind_Fountain {
 	private $system;
 
 	public function __construct() {
-		require_once 'Loader.php';
+			//- Include the Suskind_Loader class to define automatic loader methods.
+		require_once $_ENV['PATH_SYSTEM'].'/Suskind/Loader.php';
 		$this->loader = Suskind_Loader::getInstance();
-		
 		$this->system = Suskind_System::getInstance();
 	}
 }
