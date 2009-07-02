c26b54db876b1c6b89c8b41a493989f698d0df84
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-02 15:02:01 +0000

    - Configuration folder added to store application's configurations.  - Registry handler writing began, to handle these configuration files.  - Application updated.  - Loader unneccesary parts removed.

--- Suskind/Registry.php
+++ Suskind/Registry.php
@@ -7,12 +7,34 @@
 /**
  * Description of Registry
  *
- * @author bercsey
+ * @author Balazs Ercsey <laze@laze.hu>
  */
 class Suskind_Registry {
-    //put your code here
+	
+	/**
+	 * The registry values
+	 *
+	 * @var array
+	 */
+	private $registry = array();
+
 	public function  __construct() {
-		echo('Hi, this is registry');
+		$this->loadFile();
+	}
+
+	/**
+	 * Loading different configuration files:
+	 * - First try to get the application's global configuration.
+	 *
+	 * @param string $filename
+	 */
+	public function loadFile(string $filename = null) {
+			//- Gets the application's global configuration.
+		if (file_exists($_ENV['PATH_APPLICATION'].'/Configuration/application.ini')) array_merge_recursive($this->registry, parse_ini_file($_ENV['PATH_APPLICATION'].'/Configuration/application.ini', true));
+			//- Gets the application's host-related configuration.
+		if (file_exists($_ENV['PATH_APPLICATION'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini')) array_merge_recursive($this->registry, parse_ini_file($_ENV['PATH_APPLICATION'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini', true));
+			//- Add advanced configurations to registry.
+		if (!is_null($filename) && file_exists($filename)) array_merge_recursive($this->registry, parse_ini_file($filename, true));
 	}
 }
 ?>
