cf01a714e8594ac9a811b8b4677835a223ff499d
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-03 14:04:05 +0000

    - More directories added for test application. And, yes, it looks like an MVC framework soon... - Added a demo model for testing, called Guestbook. - Registry handler updated.

---
+++ Suskind/Model.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Model
+ *
+ * @author Balazs Ercsey <laze@laze.hu>
+ */
+class Suskind_Model {
+    //put your code here
+}
+?>
