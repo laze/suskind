6f57f31a44271a8310a9f51b175b159995df138f
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-07 14:25:46 +0000

    - Creating registry...

--- trunk/Suskind/Registry.php
+++ trunk/Suskind/Registry.php
@@ -22,6 +22,37 @@ class Suskind_Registry {
 		$this->load();
 	}
 
+	public function __get($property) {
+	}
+
+
+	public function getGroup($group) {
+		if (strpos($group, '.')) {
+			list ($parent, $key) = explode('.', $group, 2);
+			if (array_key_exists($parent, $this->registry)) {
+				if (array_key_exists($key, $this->registry[$parent])) return $this->registry[$parent][$key];
+				else return false;
+			} else return false;
+		} else {
+			if (array_key_exists($group, $this->registry)) return $this->registry[$group];
+			else return false;
+		}
+	}
+
+	public function checkGroup($group) {
+		var_dump(array_key_exists($group, $this->registry));
+		if (strpos($group, '.')) {
+			list ($parent, $key) = explode('.', $group, 2);
+			if (array_key_exists($parent, $this->registry)) {
+				if (array_key_exists($key, $this->registry[$parent])) return true;
+				else return false;
+			} else return false;
+		} else {
+			if (array_key_exists($group, $this->registry)) return true;
+			else return false;
+		}
+	}
+
 	/**
 	 * Loading different configuration files: First of all try to get the
 	 * application's global configuration. After that, try to get the
@@ -31,10 +62,10 @@ class Suskind_Registry {
 	 * @return void
 	 */
 	public function load() {
-			//- Gets the application's global configuration.
-			if (file_exists($_ENV['PATH_APPLICATION'].'/Configuration/application.ini')) $this->addRegistry(parse_ini_file($_ENV['PATH_APPLICATION'].'/Configuration/application.ini', true));
-			//- Gets the application's host-related configuration.
-			if (file_exists($_ENV['PATH_APPLICATION'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini')) $this->addRegistry(parse_ini_file($_ENV['PATH_APPLICATION'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini', true));
+		//- Gets the application's global configuration.
+		if (file_exists($_ENV['PATH_APPLICATION'].'/Configuration/application.ini')) $this->addRegistry(parse_ini_file($_ENV['PATH_APPLICATION'].'/Configuration/application.ini', true));
+		//- Gets the application's host-related configuration.
+		if (file_exists($_ENV['PATH_APPLICATION'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini')) $this->addRegistry(parse_ini_file($_ENV['PATH_APPLICATION'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini', true));
 	}
 
 	/**
@@ -42,7 +73,7 @@ class Suskind_Registry {
 	 * new registry from a specified file. This method DOES NOT write over the
 	 * registry, these options are just will be added to it.
 	 *
-	 * @param string $filename
+	 * @param string $filename The path of the file to load.
 	 * @return void
 	 */
 	public function loadFile(string $filename) {
@@ -50,17 +81,22 @@ class Suskind_Registry {
 		else throw new Suskind_Exception(1001,array($filename));
 	}
 
+	/**
+	 * Add settings to the registry.
+	 *
+	 * @param array $configuration A previously parsed ini file.
+	 * @return void
+	 */
 	private function addRegistry(array $configuration) {
 		foreach ($configuration as $key => $value) {
-			var_dump($value);
 			if(strpos($key, '.') !== false) {
 				list($parent, $folder) = explode('.', $key);
 				$this->registry[$parent][$folder] = $value;
 			} else $this->registry[$key] = $value;
 		}
-		echo('<pre>');
+		echo( '<pre>' );
 		var_dump($this->registry);
-		echo('</pre>');
+		echo( '</pre>' );
 	}
 }
 ?>
