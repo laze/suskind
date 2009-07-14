21e6ed711a5aef10d65e518b80fd936fad923c04
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-14 15:07:43 +0000

    - Registry handler update - Router loads path definitions

--- Suskind/Fountain.php
+++ Suskind/Fountain.php
@@ -18,18 +18,15 @@ final class Suskind_Fountain {
 
 	private $system;
 
-	public $router;
-
 	public function __construct() {
 			//- Include the Suskind_Loader class to define automatic loader methods.
 		require_once $_ENV['PATH_SYSTEM'].'/Suskind/Loader.php';
 		$this->loader = Suskind_Loader::getInstance();
 		$this->system = Suskind_System::getInstance();
-		$this->router = Suskind_Router::getInstance();
 	}
 
 	public function getRoute() {
-		$this->router->parseRoute();
+		$this->system->router->parseRoute();
 	}
 }
 
