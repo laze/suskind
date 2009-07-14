ea23f6acac8136df9dd64da768fa9a1f27d95efd
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-14 21:45:43 +0000

    - New directories added. - Renders' exception added.

---
+++ Suskind/Render/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * License
+ */
+
+/**
+ * Description of Loader
+ *
+ * @author Balazs Ercsey <laze@laze.hu>
+ */
+class Suskind_Render_Exception extends Suskind_Exception {
+}
+
+?>
