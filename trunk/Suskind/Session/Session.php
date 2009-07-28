b30cb6e09cfca49ac68d1cd5c45d4f76aa95bd1d
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-07-28 14:58:03 +0000

    - Session handling updates. - Registry extends...

--- trunk/Suskind/Session/Session.php
+++ trunk/Suskind/Session/Session.php
@@ -9,33 +9,25 @@
  * @author Balazs Ercsey <laze@laze.hu>
  */
 class Suskind_Session_Session implements Suskind_Session_Interface {
-	private $save_path;
-	private static $sessionId;
-	private static $valid;
+	private static $path;
+	private static $session = array();
 
-   	public static function open($save_path, $session_name) {
-		global $sess_save_path;
+	public static function open($path, $session_name) {
+		self::$path = $path;
 
-		$sess_save_path = $save_path;
 		return (true);
 	}
 
 	public static function close() {
-		return(true);
+		return (true);
 	}
 
 	public static function read($id) {
-		global $sess_save_path;
-
-		$sess_file = "$sess_save_path/sess_$id";
-		return (string) @file_get_contents($sess_file);
+		return (string) @file_get_contents(self::$path.'/sess_'.$id);
 	}
 
 	public static function write($id, $sess_data) {
-		global $sess_save_path;
-
-		$sess_file = "$sess_save_path/sess_$id";
-		if ($fp = @fopen($sess_file, "w")) {
+		if ($fp = @fopen(self::$path.'/sess_'.$id, "w")) {
 			$return = fwrite($fp, $sess_data);
 			fclose($fp);
 			return $return;
