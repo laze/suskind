deab8f282b82c9b146605e8ad5d355050541a002
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-08-19 13:58:46 +0000

    - I begin to define views...

---
+++ trunk/Suskind/View/Static/Interface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * License
+ */
+
+/**
+ * Suskind_View_Interface interface
+ *
+ * @package     Suskind
+ * @package     View
+ * @author		Balazs Ercsey <laze@laze.hu>
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html GPLv3
+ * @link        http://code.google.com/p/suskind
+ * @since       0.1
+ * @version
+ */
+interface Suskind_View_Static_Interface extends Suskind_View_Interface {
+}
+
+?>
