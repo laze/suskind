0fd310beefe33a6a3d1929d566c97ea07f271255
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-08-28 22:23:50 +0000

    - Application initialization updates.

--- trunk/Apps/Test/Public/index.php
+++ trunk/Apps/Test/Public/index.php
@@ -9,34 +9,7 @@
 require_once realpath('../Library').'/Application.php';
 
 $application = new Suskind_Application();
-$application->run();
-
-$_SESSION['visit']++;
-var_dump($_SESSION);
-
-//// Define path to application directory
-//defined('APPLICATION_PATH')
-//    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-//
-//// Define application environment
-//defined('APPLICATION_ENV')
-//    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-//
-//// Ensure library/ is on include_path
-//set_include_path(implode(PATH_SEPARATOR, array(
-//    realpath(APPLICATION_PATH . '/../library'),
-//    get_include_path(),
-//)));
-//
-///** Zend_Application */
-//require_once 'Zend/Application.php';
-//
-//// Create application, bootstrap, and run
-//$application = new Zend_Application(
-//    APPLICATION_ENV,
-//    APPLICATION_PATH . '/configs/application.ini'
-//);
-//$application->bootstrap()
-//            ->run();
+$application->init()
+			->show();
 
 ?>
