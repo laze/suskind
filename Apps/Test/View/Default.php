6df1e02ec6b336b7b6982f8b98ed3dcd896280ae
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-08-30 16:30:24 +0000

    - A default view added for testing.

---
+++ Apps/Test/View/Default.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Default
+ *
+ * @author Balazs Ercsey <laze@laze.hu>
+ */
+class Application_View_Default extends Suskind_View_General {
+}
+
+?>
