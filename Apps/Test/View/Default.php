7b39acbcdad6e171efd770af2447b1862067f8b7
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-03 13:42:09 +0000

    - Application's default view added.

--- Apps/Test/View/Default.php
+++ Apps/Test/View/Default.php
@@ -9,7 +9,15 @@
  *
  * @author Balazs Ercsey <laze@laze.hu>
  */
-class Application_View_Default extends Suskind_View_General {
+class Application_View_Default extends Suskind_View_View {
+
+	/**
+	 * Construction of template.
+	 */
+	public function __construct() {
+		$this->class = __CLASS__;
+		parent::__construct();
+	}
 }
 
 ?>
