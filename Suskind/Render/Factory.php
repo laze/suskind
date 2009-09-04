b960405151f5e04fbf3094bead5eb1695f572731
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-04 14:44:37 +0000

    - Testing...

--- Suskind/Render/Factory.php
+++ Suskind/Render/Factory.php
@@ -11,7 +11,15 @@
  */
 class Suskind_Render_Factory {
 	public static function createRender() {
-		return ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) ? new Suskind_Render_Json() : new Suskind_Render_Html();
+		$registry = Suskind_Registry::getSettings('render');
+
+		if ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
+			if (is_array($registry) && array_key_exists('json', $registry)) return new $registry['json']();
+			else return new Suskind_Render_Json();
+		} else {
+			if (is_array($registry) && array_key_exists('html', $registry)) return new $registry['html']();
+			else return new Suskind_Render_Html();
+		}
 	}
 }
 
