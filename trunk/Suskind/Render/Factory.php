e8b5f91f95005a2747ea8ed555d1649dd43101eb
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-05 16:33:19 +0000

    - The render factory method upgraded to be able to handle every possible outputs. Currently it just useable for html, ajax, but later - I suppose - here can we define more outputs, like xml, pdf, Excel sheet, etc. - A render plugin created to use the famous Template_Lite engine.

--- trunk/Suskind/Render/Factory.php
+++ trunk/Suskind/Render/Factory.php
@@ -10,15 +10,21 @@
  * @author Balazs Ercsey <laze@laze.hu>
  */
 class Suskind_Render_Factory {
-	public static function createRender() {
-		$registry = Suskind_Registry::getSettings('render');
+	private static $renders = array(
+		'html'	=> 'Suskind_Render_Html',
+		'ajax'	=> 'Suskind_Render_Json',
+		'xml'	=> 'Suskind_Render_Xml'
+	);
+	private static $defaultRender = 'html';
 
-		if ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
-			if (is_array($registry) && array_key_exists('json', $registry)) return new $registry['json']();
-			else return new Suskind_Render_Json();
-		} else {
-			if (is_array($registry) && array_key_exists('html', $registry)) return new $registry['html']();
-			else return new Suskind_Render_Html();
+	public static function createRender($type = null) {
+		if (!is_null($type) && array_key_exists($type, self::$renders)) return new self::$renders[$type]();
+		else {
+			$render = (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') ? 'ajax' : self::$defaultRender;
+			self::$renders = array_merge(self::$renders, Suskind_Registry::getSettings('render'));
+			foreach (Suskind_Registry::getSettings('render') as $renderType => $renderClass)
+				if (substr($renderClass, 0, 7) != 'Suskind') self::$renders[$renderType] = 'Suskind_Render_Plugin_'.str_replace('_', '', $renderClass);
+			return new self::$renders[$render]();
 		}
 	}
 }
---
+++ trunk/Suskind/Render/Plugin/TemplateLite.php
@@ -0,0 +1,62 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Plugin
+ *
+ * @author laze
+ */
+class Suskind_Render_Plugin_TemplateLite implements Suskind_Render_Interface {
+	/**
+	 * The object, what is defined in the registry. This object
+	 * @var object
+	 */
+	private $engine;
+	private $cache = 'suskind';
+
+	/**
+	 * The path to template file.
+	 * 
+	 * @var string
+	 */
+	private $template;
+
+	public function __construct() {
+		$this->engine = new Template_Lite();
+		/**
+		 * Setting up the Template Lite engine.
+		 */
+		$this->engine->left_delimiter = '<:';
+		$this->engine->right_delimiter =':>';
+		$this->engine->cache = true;
+		$this->engine->template_dir = $_ENV['PATH_APPLICATION'].DIRECTORY_SEPARATOR.'Assets'.DIRECTORY_SEPARATOR.'Templates';
+		$this->engine->cache_dir = $_ENV['PATH_APPLICATION'].DIRECTORY_SEPARATOR.'Assets'.DIRECTORY_SEPARATOR.'Templates'.DIRECTORY_SEPARATOR.'Cached';
+		$this->engine->compile_dir = $_ENV['PATH_APPLICATION'].DIRECTORY_SEPARATOR.'Assets'.DIRECTORY_SEPARATOR.'Templates'.DIRECTORY_SEPARATOR.'Compiled';
+		/**
+		 * Assigning default variables.
+		 */
+		$this->engine->assign('application', Suskind_Registry::getSettings('Suskind_Application'));
+	}
+
+	public function assign($variableName, $variableValue, $variableModification = null) {
+		$this->engine->assign($variableName, $variableValue);
+	}
+
+	public function remove($variableName) {
+		$this->engine->clear_assign($variableName);
+	}
+
+	public function compile() {
+		return $this->engine->fetch($this->template, $this->cache);
+	}
+
+	public function setTemplate($fileName) {
+		if (file_exists($fileName)) $this->template = $fileName;
+	}
+
+}
+
+?>
