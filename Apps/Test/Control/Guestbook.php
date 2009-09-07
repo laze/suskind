7a3bc40195643986996dd575f70b2cdaeba2f81e
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-07 14:42:51 +0000

    - Trying to find out the relationship between events and views.

--- Apps/Test/Control/Guestbook.php
+++ Apps/Test/Control/Guestbook.php
@@ -10,15 +10,18 @@
  * @author laze
  */
 class Application_Control_Guestbook extends Suskind_Control_Control {
-	private $model;
 
 	public function __construct() {
 		$this->model = new Application_Model_Guestbook();
 	}
 
 	public function getDefaultView() {
-		echo __CLASS__.'_Default';
-		return new Suskind_View_View(__CLASS__.'_Default');
+		var_dump(__CLASS__);
+		$viewClass = __CLASS__.'_'.$this->defaultView;
+		var_dump($viewClass);
+		if (class_exists($viewClass)) {
+			return new $viewClass;
+		} else return new Suskind_View_View(str_replace('Control', 'View', __CLASS__.'_Default'));
 	}
 }
 ?>
