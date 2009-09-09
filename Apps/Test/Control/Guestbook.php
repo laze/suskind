fbc25a2f85f881a656e7e1138c1e29268be67b81
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-09 15:24:22 +0000

    - Control updates for testing.

--- Apps/Test/Control/Guestbook.php
+++ Apps/Test/Control/Guestbook.php
@@ -17,12 +17,7 @@ class Application_Control_Guestbook extends Suskind_Control_Control {
 			'Add'
 		);
 		$this->model = new Application_Model_Guestbook();
-	}
-
-	public function getDefaultView() {
-		$viewClass = str_replace('Control', 'View', __CLASS__.'_'.$this->events[0]);
-		if (class_exists($viewClass)) return new $viewClass($viewClass);
-		else return new Suskind_View_View(str_replace('Control', 'View', __CLASS__.'_Default'));
+		$this->control = __CLASS__;
 	}
 }
 ?>
