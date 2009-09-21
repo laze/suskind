a3528af751d60e8a187f5490521b6ad6303c7415
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-21 08:40:58 +0000

    - Applications' folder changed, to be easier to install, without any symlinks...

---
+++ trunk/Suskind/Applications/Test/View/Guestbook/List.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of List
+ *
+ * @author bercsey
+ */
+class Application_View_Guestbook_List extends Suskind_View_View {
+	public function compile() {
+//		$
+		parent::$render->compile();
+	}
+}
+
+?>
