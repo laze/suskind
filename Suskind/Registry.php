6ff429e4185729f4a150dd5f99de179035cd4771
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-29 14:38:04 +0000

    - Registry updated.

--- Suskind/Registry.php
+++ Suskind/Registry.php
@@ -10,24 +10,23 @@
  * @author Balazs Ercsey <laze@laze.hu>
  */
 class Suskind_Registry {
+
+	const CKEY_SYSTEM	= 'Suskind';
+	const CKEY_APP		= 'Application';
+	const CKEY_SERVER	= 'Server';
+	const CKEY_USER		= 'Client';
+	const CKEY_PLUGIN	= 'Plugin';
+	const CKEY_RESOURCE	= 'Resource';
+	const CKEY_RENDER	= 'Render';
+	const CKEY_CONFIG	= 'Configuration';
+	const CKEY_STORE	= 'Store';
 	
 	/**
 	 * The registry values.
 	 *
 	 * @var array
 	 */
-	private static $registry = array(
-		'System'		=> array(
-			'Server'		=> array(),
-			'Path'			=> array(),
-			'Client'		=> array()
-		),
-		'Application'	=> array(
-			'Plugins'		=> array(),
-			'Render'		=> array(),
-			'Resource'		=> array()
-		)
-	);
+	private static $registry = array();
 
 	/**
 	 * Build up the registry. This method call other methods, to calculate paths,
@@ -84,10 +83,33 @@ class Suskind_Registry {
 	 * @return void
 	 */
 	private static function loadPaths(array $paths) {
-		self::$registry['System']['Path'] = $paths;
-		echo('<pre>');
-		var_dump(self::$registry);
-		echo('</pre>');
+		self::$registry[self::CKEY_SYSTEM]['Path'] = $paths[self::CKEY_SYSTEM];
+		self::$registry[self::CKEY_APP]['Path'] = $paths[self::CKEY_APP];
+
+		self::$registry[self::CKEY_APP][self::CKEY_PLUGIN]['Path'] = self::checkPath(self::CKEY_PLUGIN);
+		self::$registry[self::CKEY_APP][self::CKEY_RESOURCE]['Path'] = self::checkPath(self::CKEY_RESOURCE);
+		self::$registry[self::CKEY_APP][self::CKEY_RENDER]['Path'] = self::checkPath(self::CKEY_RENDER);
+
+		self::$registry[self::CKEY_APP][self::CKEY_CONFIG] = self::checkConfigurationPath(self::$registry[self::CKEY_APP]['Path'].DIRECTORY_SEPARATOR.self::CKEY_CONFIG.DIRECTORY_SEPARATOR);
+	}
+
+	private static function checkConfigurationPath($path) {
+		if (file_exists($path.$_SERVER['SERVER_NAME'].DIRECTORY_SEPARATOR.'Application.ini')) return $path.$_SERVER['SERVER_NAME'].DIRECTORY_SEPARATOR.'Configuration.ini';
+		else {
+			if ($path.'Application.ini') return $path.'Configuration.ini';
+			else throw new Suskind_Exception(Suskind_Exception_Registry::ConfigurationNotExists(self::$registry[self::CKEY_APP]['Path'].DIRECTORY_SEPARATOR.self::CKEY_CONFIG));
+		}
+	}
+
+	/**
+	 * First, try to find th folder in the Application part, and if it not exists
+	 * there, then checks the System path.
+	 *
+	 * @param string $path The name of the folder to find.
+	 * @return string
+	 */
+	private static function checkPath($path) {
+		return (file_exists(self::$registry[self::CKEY_APP]['Path'].DIRECTORY_SEPARATOR.$path)) ? self::$registry[self::CKEY_APP]['Path'].DIRECTORY_SEPARATOR.$path : self::$registry[self::CKEY_SYSTEM]['Path'].DIRECTORY_SEPARATOR.$path;
 	}
 
 	/**
@@ -99,10 +121,14 @@ class Suskind_Registry {
 	 * @return void
 	 */
 	private static function loadConfigurations() {
+		self::addRegistry(parse_ini_file(self::$registry[self::CKEY_APP][self::CKEY_CONFIG], true));
+		/*
 		//- Gets the application's global configuration.
 		if (file_exists(self::$paths['Application'].'/Configuration/application.ini')) self::addRegistry(parse_ini_file(self::$paths['Application'].'/Configuration/application.ini', true));
 		//- Gets the application's host-related configuration.
 		if (file_exists(self::$paths['Application'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini')) $this->addRegistry(parse_ini_file(self::$paths['Application'].'/Configuration/'.$_SERVER['SERVER_NAME'].'/application.ini', true));
+		 *
+		 */
 	}
 
 	/**
@@ -115,7 +141,7 @@ class Suskind_Registry {
 	 */
 	public static function loadFile(string $filename) {
 		if (file_exists($filename)) self::getInstance()->addRegistry(parse_ini_file($filename, true));
-		else throw new Suskind_Exception(1001,array($filename));
+		else throw new Suskind_Exception(Suskind_Exception_File::NotExists($filename));
 	}
 
 	/**
@@ -123,12 +149,44 @@ class Suskind_Registry {
 	 *
 	 * @param array $configuration A previously parsed ini file.
 	 * @return void
+	 *
+	 * @todo This method need more development to parse more informations, like
+	 * registry, etc.
 	 */
 	private static function addRegistry(array $configuration) {
-		foreach ($configuration as $path => $settings) {
-			$path = 'Suskind_'.str_replace(' ', '_', ucwords(str_replace('.', ' ', $path)));
-			self::$registry[$path] = $settings;
+		foreach ($configuration as $key => $settings) {
+			if (array_key_exists(trim($key), self::$registry)) {
+
+				$path = 'Suskind_'.str_replace(' ', '_', ucwords(str_replace('.', ' ', $path)));
+				self::$registry[trim($key)] = array_merge($settings, self::$registry[trim($key)]);
+			}
 		}
+	}
+
+	/**
+	 * Store a variable in the the registry's runtime store.
+	 *
+	 * @param string $name The key to store any value.
+	 * @param mixed $value The value for the name.
+	 * @param boolean $force If it's true, then doesn't check whether $name is exists or not, before writing value.
+	 * @return mixed Return the value what is set. If force was false, and key exists, then return the previously set value.
+	 */
+	public static final function setStore($name, $value = null, $force = false) {
+		if ($force === false) {
+			if (!array_key_exists($name, self::$registry[self::CKEY_STORE])) self::$registry[self::CKEY_STORE][$name] = $value;
+		} else self::$registry[self::CKEY_STORE][$name] = $value;
+		return self::$registry[self::CKEY_STORE][$name];
+	}
+
+	/**
+	 * Get a variable from the store.
+	 *
+	 * @param string $name The key to get value.
+	 * @return mixed Returns with the value, or null, if key not exists.
+	 */
+	public static final function getStore($name) {
+		if (array_key_exists($name, self::$registry[self::CKEY_STORE])) return self::$registry[self::CKEY_STORE][$name];
+		else return null;
 	}
 }
 ?>
