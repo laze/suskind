870e8fcabbe372248e787be9020469a2e1b5f977
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2009-09-30 19:44:54 +0000

    - CKEY_DB located...

--- Suskind/Registry.php
+++ Suskind/Registry.php
@@ -32,14 +32,18 @@ class Suskind_Registry {
 	private static $registryKeys = array(
 		self::CKEY_SYSTEM => array(
 			self::CKEY_RENDER => array(),
-			self::CKEY_RESOURCE => array(),
+			self::CKEY_RESOURCE => array(
+                            self::CKEY_DB => array()
+                        ),
 			self::CKEY_CONFIG => array(),
 			self::CKEY_PLUGIN => array(),
 			self::CKEY_PATH => array()
 		),
 		self::CKEY_APP => array(
 			self::CKEY_RENDER => array(),
-			self::CKEY_RESOURCE => array(),
+			self::CKEY_RESOURCE => array(
+                            self::CKEY_DB => array()
+                        ),
 			self::CKEY_CONFIG => array(),
 			self::CKEY_PLUGIN => array(),
 			self::CKEY_PATH => array()
