2dcd3b3c43d56c41bd01c65dddc678fa13ddb9e2
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2010-04-21 14:57:36 +0000

    - PHP's spl_autoload functions defined - Automatic directory recognition added. - Code documented.

---
+++ Library/Suskind/Loader.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Suskind Framework
+ *
+ * LICENSE
+ *
+ * This source file is a subject of the GPLv3 that is bundled with this package
+ * in the file License.txt
+ * It is also available at this URL:
+ * http://www.opensource.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * Loader class for handling autoloaders.
+ *
+ * @category	Suskind
+ * @package		Suskind
+ * @subpackage	Loader
+ * @author		Balazs Ercsey <laze@laze.hu>
+ * @license		http://www.opensource.org/licenses/gpl-3.0.html GPLv3
+ * @link		http://code.google.com/p/suskind/
+ * @since		0.1
+ * @version		$Rev$
+ */
+class Suskind_Loader
+{
+	/**
+	 *
+	 */
+	const DIR_APP = 'Application';
+	const DIR_SUS = 'Suskind';
+	const DIR_LIB = 'Library';
+	
+	/**
+	 * This array stores the different pathes, like the application's path, the
+	 * Suskind library's path, the global libraries' path, and also it stores
+	 * the different libraries.
+	 *
+	 * @var array $paths	Array of paths.
+	 * @static
+	 */
+	private static $paths;
+
+	/**
+	 * __construct
+	 *
+	 * @return void
+	 * @throws Suskind_Exception
+	 */
+	public function  __construct() {
+		throw Suskind_Exception::ClassStaticConstruct();
+	}
+
+	/**
+	 * Set up path variables for autoload methods.
+	 *
+	 * @return void
+	 */
+	private static function setPath() {
+		self::$paths = array(
+			self::DIR_APP => realpath(getcwd()),
+			self::DIR_SUS => realpath(dirname(__FILE__)),
+			self::DIR_LIB => realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'..')
+		);
+		$dir = dir(self::$paths[self::DIR_LIB]);
+		while (false !== ($entry = $dir->read())) {
+			if (substr($entry, 0, 1) !== '.' && !array_key_exists($entry, self::$paths)) self::$paths[$entry] = self::$paths[self::DIR_LIB].DIRECTORY_SEPARATOR.$entry;
+		}
+		$dir->close();
+	}
+
+	/**
+	 * The mean of the Loader class. This method will be called first in an
+	 * application, so this method shoud prepare everything.
+	 *  * Set up path variables
+	 *  * Register autolad handler methods.
+	 *
+	 * @see Suskind_Loader::setPath()
+	 * @see Suskind_Loader::addAutoload()
+	 * @return void
+	 */
+	static public function load() {
+		self::setPath();
+		self::addAutoload();
+	}
+
+	/**
+	 * Register Suskind_Loader in spl_autoload.
+	 *
+	 * @return void
+	 * @throws Suskind_Exception
+	 */
+	static public function addAutoload() {
+		ini_set('unserialize_callback_func', 'spl_autoload_call');
+
+		if (false === spl_autoload_register(array(self, 'autoload')))  throw new Suskind_Exception(sprintf('Unable to register %s::autoload as an autoloading method.', __CLASS__));
+	}
+
+	/**
+	 * Unregister Suskind_Loader from spl_autoload.
+	 *
+	 * @return void
+	 */
+	static public function removeAutoload() {
+		spl_autoload_unregister(array(self::getIntance(), 'autoload'));
+	}
+
+	/**
+	 * Calculate the path from the class' name.
+	 *
+	 * @param string $class					The name of the class what we want include.
+	 * @return true|void					True, if class or interface previously included.
+	 * @throws Suskind_Exception
+	 */
+	public static function includeClass($class) {
+		if (class_exists($class, false) || interface_exists($class, false)) return true;
+		
+		$path = explode('_', $class);
+		$path[0] = self::$paths[$path[0]];
+
+		if (file_exists(implode(DIRECTORY_SEPARATOR, $path).'.php')) require_once implode(DIRECTORY_SEPARATOR, $path).'.php';
+		else throw new Suskin_Exception();
+	}
+
+	/**
+	 * Gets name of class and include file from possible pathes.
+	 *
+	 * @param string $class					Name of the class to include.
+	 * @return void
+	 */
+	public static function autoload($class) {
+		return self::includeClass($class);
+	}
+
+}
+?>
