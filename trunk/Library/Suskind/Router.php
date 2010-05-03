d67ad722821bbb6729ffd30118e1eea610ca0b8b
ercseybalazs <ercseybalazs@d888ae48-6a54-0410-9723-ebdb137dc43f>
2010-05-03 06:13:50 +0000

    - Variable check fix.

--- trunk/Library/Suskind/Router.php
+++ trunk/Library/Suskind/Router.php
@@ -54,8 +54,6 @@ class Suskind_Router
 	public function __construct($uri = null) {
 		$this->registry = Suskind_Loader::loadConfiguration('Routing.yml');
 		if (is_array($uri)) $this->setDirective($uri);
-
-		var_dump($this->directive);
 	}
 
 	public function setDirective($uri) {
@@ -90,7 +88,7 @@ class Suskind_Router
 	 * @return null|string		Param's array value of "name" key...
 	 */
 	public function getParam($key) {
-		return (array_key_exists($key, $this->directive['param'])) ? $this->directive['param'][$key] : null;
+		return (is_array($this->directive['param']) && array_key_exists($key, $this->directive['param'])) ? $this->directive['param'][$key] : null;
 	}
 
 }
